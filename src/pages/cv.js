2636b205654bb934bdafd4d7bf9daba6ee8f48ae
Pablo Vila <pablo.vilafer@gmail.com>
2019-02-03 19:29:52 +0100

    added option to see cv code

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -1,26 +1,72 @@
-import React from 'react'
-import SEO from '../components/seo'
+import React, { Component } from 'react'
+import Highlight from 'react-highlight'
 
+import SEO from '../components/seo'
 import Header from '../components/cv/Header'
 import Experience from '../components/cv/Experience'
 import Skills from '../components/cv/Skills'
 import Education from '../components/cv/Education'
 
+import CvJson from '../data/cv.json'
+
 import '../styles/cv.css'
+import 'highlight.js/styles/github.css'
 
-const cv = () => (
-  <>
-    <SEO
-      title="resume"
-      keywords={[`resume`, `cv`, `curriculum vitae`, `curriculum`]}
-    />
-    <div className="cv-page">
-      <Header />
-      <Experience />
-      <Skills />
-      <Education />
-    </div>
-  </>
-)
+class Cv extends Component {
+  state = {
+    codeView: false,
+  }
 
-export default cv
+  constructor() {
+    super()
+    this.onViewCodeClick = this.onViewCodeClick.bind(this)
+  }
+
+  onViewCodeClick() {
+    this.setState(state => ({
+      codeView: !state.codeView,
+    }))
+  }
+
+  render() {
+    console.log(CvJson)
+    let cv = <></>
+    if (this.state.codeView) {
+      cv = (
+        <>
+          <div className="cv-json-container">
+            <Highlight className="json">
+              {JSON.stringify(CvJson, null, '\t')}
+            </Highlight>
+          </div>
+          <div className="cv-switcher" onClick={this.onViewCodeClick}>
+            View CV
+          </div>
+        </>
+      )
+    } else {
+      cv = (
+        <>
+          <Header />
+          <Experience />
+          <Skills />
+          <Education />
+          <div className="cv-switcher" onClick={this.onViewCodeClick}>
+            View code
+          </div>
+        </>
+      )
+    }
+
+    return (
+      <>
+        <SEO
+          title="resume"
+          keywords={[`resume`, `cv`, `curriculum vitae`, `curriculum`]}
+        />
+        <div className="cv-page">{cv}</div>
+      </>
+    )
+  }
+}
+export default Cv
